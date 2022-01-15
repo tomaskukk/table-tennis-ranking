5312469b643c9355f73584b365491282ac555921
tomaskukk <tomsu_kukk@hotmail.com>
2022-01-15 18:32:02 +0200

    remove dead files

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,11 @@ import { jsx } from 'theme-ui';
 import type { NextPage } from 'next';
 import Button from '../src/components/Button';
 
-const players = ['Tomas', 'Jarkko', 'Noah'];
-
 const Home: NextPage = () => {
   return (
     <div>
       <h1>Table tennis ranking system</h1>
-      <Button>Click me men</Button>
+      <Button>Nice button</Button>
     </div>
   );
 };
