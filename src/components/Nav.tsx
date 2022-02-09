3c8fbae981551ff9174f8908a58a7668b3bbc1ba
Noah <noahnettey@gmail.com>
2022-02-09 23:33:30 +0200

    chore: refactor nav with styled-components and make it look better

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,35 +1,38 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
+import { FC } from 'react';
+
 import Link from 'next/link';
+import styled from 'styled-components';
 
-const Nav = () => (
-  <header sx={{ height: '60px', width: '100vw', variant: 'containers.page' }}>
-    <nav
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
+const StyledNav = styled.div`
+  margin-top: -1.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  letter-spacing: 0.25rem;
+  background: rgba(7, 6, 19, 1); //radial-gradient(ellipse at top, rgba(7,6,19, 0.95) 0%, rgba(7,6,19, 1) 60%);
+`;
 
-        height: '100%',
-        '> *': {
-          marginLeft: '2rem',
-        },
-      }}
-    >
-      <p sx={{ fontSize: '30px' }}>🏓</p>
-      <Link href="/">
-        <a sx={{ fontWeight: 'bold', fontSize: 4, cursor: 'pointer' }}>Home</a>
-      </Link>
-
-      <Link href="/players">
-        <a sx={{ color: 'text', fontSize: 3, cursor: 'pointer' }}>Players</a>
-      </Link>
-
-      <Link href="/matches">
-        <a sx={{ color: 'text', fontSize: 3, cursor: 'pointer' }}>Matches</a>
-      </Link>
-    </nav>
-  </header>
+const NavButton = styled.div`
+  padding: 0.5rem 2rem;
+  display: flex;
+  justify-content: flex-start;
+  font-weight: bold;
+  text-transform: uppercase;
+  cursor: pointer;
+`;
+export const Nav: FC<{}> = () => (
+  <StyledNav>
+    <NavButton>
+      <Link href="/">Home</Link>
+    </NavButton>
+    <NavButton>
+      <Link href="/players">Players</Link>
+    </NavButton>
+    <NavButton>
+      <Link href="/matches">Matches</Link>
+    </NavButton>
+  </StyledNav>
 );
-
-export default Nav;
