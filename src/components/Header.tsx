57d2a358b5481fc02404bc7022acdfa931f518fc
Noah <noahnettey@gmail.com>
2022-05-17 20:34:27 +0300

    chore: adjust header and logo size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ const StyledHeader = styled.div`
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 12rem;
+  height: 16rem;
   text-align: center;
   background: linear-gradient(180deg, transparent 0%, rgba(7, 6, 19, 0.95) 80%, rgba(7, 6, 19, 1) 90%),
     radial-gradient(ellipse at top, rgba(7, 6, 19, 0.9) 0%, rgba(7, 6, 19, 0.95) 99%, rgba(7, 6, 19, 1) 100%);
